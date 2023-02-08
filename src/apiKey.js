baadb9034a14c1ff7fa53f33c46d19ef0681ac1d
daniel matama <danmatama2020@gmail.com>
2023-02-08 14:39:48 +0300

    Work on popup

---
+++ src/apiKey.js
@@ -0,0 +1,3 @@
+const apiKey = 'BDiHo1eWo7TvFu0NP10u';
+
+export default apiKey;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.scss';
+
+// const cardButton = document.querySelector('#card-button');
+
+// cardButton.addEventListener('click',()=>{
+//     console.log('card button clicked')
+// });
