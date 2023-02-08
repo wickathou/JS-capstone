6e48084c7eacbd4e1684cf90afb5060f79b20f1f
Javier <javidx@gmail.com>
2023-02-08 14:28:01 +0200

    Added: Class constructor for shows and shows with new methods Improved tvmaze functions to update data on the class instances

--- src/shows.js
+++ src/shows.js
@@ -1,10 +1,21 @@
+import Show from './showClass.js';
+import {showInfo} from './tvmaze.js';
 export default class Shows {
   constructor () {
     this.shows = []
   }
 
   addNew (title) {
+    const showTitle = 
     getShow(title)
+    const newShow = new Show()
+  }
+
+  generateInitial (showList) {
+    showList.forEach(async showTitle => {
+      showInfo(showTitle)
+      this.addNew()
+    });
   }
 }
 
